c8b89f92e39d438f9e16c8b4e0e6d3584fe86742
DavisLima81 <davislima81@yahoo.com.br>
2022-05-08 09:58:36 -0300

    sisgoa primeiro commit: criou sistema, instalou UI, fez primeira migration

---
+++ resources/views/home.blade.php
@@ -0,0 +1,20 @@
+@extends('adminlte::page')
+
+@section('title', 'SisGoa')
+
+@section('content_header')
+    <h1 class="m-0 text-dark">Painel principal</h1>
+@stop
+
+@section('content')
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <p class="mb-0">Login realizado!</p>
+                    <p class="mb-0">Bem vindo ao SisGoa. Faça VOAR</p>
+                </div>
+            </div>
+        </div>
+    </div>
+@stop
